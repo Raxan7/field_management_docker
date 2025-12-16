4aae05b8cfd35d1c691b386fdb97b62d7b57d87d
Raxan7 <manyerere201@gmail.com>
2025-12-16 12:56:42 +0300

    Added dbslite

--- field_management/settings.py
+++ field_management/settings.py
@@ -99,17 +99,23 @@ WSGI_APPLICATION = 'field_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'field_db',
+#         'USER': 'hajiuser',
+#         'PASSWORD': 'hajipassword',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'field_db',
-        'USER': 'hajiuser',
-        'PASSWORD': 'hajipassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
